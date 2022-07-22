2d0288eb39ce665fdf0db5cbd962b989ff1c9cfa
Onur Özgüzel <dark.punish@gmail.com>
2022-07-22 16:21:22 +0300

    First Init

---
+++ MinimalApiPOC/SixMinAPI/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SixMinAPI.Models;
+
+namespace SixMinAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        //public DbSet<Command> Commands { get; set; }
+        public DbSet<Command> Commands => Set<Command>();
+    }
+}
---
+++ MinimalApiPOC/SixMinAPI/Data/ICommandRepo.cs
@@ -0,0 +1,13 @@
+﻿using SixMinAPI.Models;
+
+namespace SixMinAPI.Data
+{
+    public interface ICommandRepo
+    {
+        Task SaveChangesAsync();
+        Task<Command?> GetCommandById(int id);
+        Task<IEnumerable<Command>> GetAllCommands();
+        Task CreateCommand(Command cmd);
+        void DeleteCommand(Command cmd);
+    }
+}
